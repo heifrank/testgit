c6d61060e6dfc0b9e21d6782134288c59c447729
heifrank <heifrank27@gmail.com>
2014-11-28 09:31:21 +0800

    second commit

---
+++ c.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: c.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: Fri 28 Nov 2014 09:30:36 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	cout << "What???" << endl;
+	return 0;
+}
