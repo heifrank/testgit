b654c925fec5901b72d244f5a80b6abdcbab7608
heifrank <heifrank27@gmail.com>
2014-11-28 09:27:27 +0800

    first commit

---
+++ a.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: a.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: Fri 28 Nov 2014 09:25:59 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << "first time trying testing git." << endl;
+	return 0;
+}
